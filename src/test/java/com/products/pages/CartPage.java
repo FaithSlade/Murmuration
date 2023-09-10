d60a3a66c120c4410c4d045cf65cdc9810eefe08
FaithSlade <faithwslade@gmail.com>
2023-09-09 17:01:31 -0700

    reorganize cartPage, Added and remove StepDef, and Add and remove feature

--- src/test/java/com/products/pages/CartPage.java
+++ src/test/java/com/products/pages/CartPage.java
@@ -51,6 +51,9 @@ public class CartPage extends BasePage{
     @FindBy(name = "remove-test.allthethings()-t-shirt-(red)")
     public WebElement allThingsRemove;
 
+    @FindBy(xpath = "//button[.='Add to cart']")
+    public List<WebElement> addToCartButton;
+
     /**
      * This method will remove List<WebElement> randomly by using Random class
      * if condition to ensure that I am not click more elements than are available.
@@ -68,6 +71,25 @@ public class CartPage extends BasePage{
         for (int i = 0; i < numberOfClick; i++) {
             int randomIndex = random.nextInt(removeItemsButton.size()); // add a random index
             WebElement element = removeItemsButton.get(randomIndex); //retrieve webElement from index
+            element.click();
+            //BrowserUtil.waitFor(2); just for check to see it click
+        }
+    }
+
+    /**
+     * This method will add List<WebElement> randomly by using Random class
+     * if condition to ensure that I am not click more elements than are available.
+     * This loop will iterate base on numberOfClick times.
+     * Then generates a random index from List<WebElement>, and used index to select an element.
+     */
+    public void addRandomElement (List<WebElement> addToCartButton,int numberOfClick) {
+        if (numberOfClick > addToCartButton.size()) {
+            numberOfClick = addToCartButton.size();
+        }
+        Random random = new Random();
+        for (int i = 0; i < numberOfClick; i++) {
+            int randomIndex = random.nextInt(addToCartButton.size()); // add a random index
+            WebElement element = addToCartButton.get(randomIndex); //retrieve webElement from index
             element.click();
             //BrowserUtil.waitFor(2); just for check to see it click
         }
--- src/test/java/com/products/stepDefinitions/AddedAndRemoved.java
+++ src/test/java/com/products/stepDefinitions/AddedAndRemoved.java
@@ -1,5 +1,6 @@
 package com.products.stepDefinitions;
 
+import com.products.pages.CartPage;
 import com.products.pages.LoginPage;
 import com.products.pages.ProductPage;
 import com.products.utilities.ConfigurationReader;
@@ -15,14 +16,20 @@ public class AddedAndRemoved {
 
     ProductPage productPage = new ProductPage();
     LoginPage loginPage = new LoginPage();
+    CartPage cartPage = new CartPage();
 
     @Given("user is on the product page")
     public void user_is_on_the_product_page() {
         loginPage.UserLogin();
     }
 
-    @When("user click items Add to cart button")
-    public void user_click_items_add_to_cart_button() {
+    @When("user randomly add {int} items and click add to cart")
+    public void user_randomly_add_items_and_click_add_to_cart(Integer expectedAddNum) {
+        cartPage.addRandomElement(cartPage.addToCartButton, expectedAddNum);
+    }
+
+    @When("user click all items Add to cart button")
+    public void user_click_all_items_add_to_cart_button() {
         for (WebElement eachItem : productPage.addToCartButton) {
             eachItem.click();
         }
@@ -36,12 +43,12 @@ public class AddedAndRemoved {
     @Then("the shopping cart should show all added items {int} in the cart")
     public void the_shopping_cart_should_show_all_added_items_in_the_cart(Integer number) {
         String expectedNum = Integer.toString(number);
-        Assert.assertEquals(expectedNum,productPage.shoppingCartBadge.getText());
+        Assert.assertEquals(expectedNum, productPage.shoppingCartBadge.getText());
     }
 
     @When("user click remove items button")
     public void user_click_remove_items_button() {
-        user_click_items_add_to_cart_button();
+        user_click_all_items_add_to_cart_button();
         for (WebElement eachItem : productPage.removeButton) {
             eachItem.click();
         }
