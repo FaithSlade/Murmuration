52d524043c020becd8766b2980559947e880aec8
FaithSlade <faithwslade@gmail.com>
2023-09-09 23:21:23 -0700

    implements userInfoPage, userInfo StepDef, and userInfo feature

--- src/test/java/com/products/stepDefinitions/UserInfo.java
+++ src/test/java/com/products/stepDefinitions/UserInfo.java
@@ -1,48 +1,67 @@
 package com.products.stepDefinitions;
 
+import com.products.pages.UserInfoPage;
+import com.products.utilities.BrowserUtil;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class UserInfo {
 
-    @When("user enter {string} as first name and {string} as last name")
-    public void user_enter_as_first_name_and_as_last_name(String firstName, String LastName) {
+    UserInfoPage userInfoPage = new UserInfoPage();
 
+    @When("user enter {string} as first name and {string} as last name")
+    public void user_enter_as_first_name_and_as_last_name(String firstName, String lastName) {
+        userInfoPage.firstName.sendKeys(firstName);
+        userInfoPage.lastName.sendKeys(lastName);
     }
+
     @When("user enter valid zip code {int}")
     public void user_enter_valid_zip_code(Integer zipCode) {
-
-    }
-    @Then("user should be able to click continue button and landing to checkout overview page")
-    public void user_should_be_able_to_click_continue_button_and_landing_to_checkout_overview_page() {
-
+        userInfoPage.zipCode.sendKeys(zipCode.toString());
     }
 
-    @When("user should not be able to click continue button")
-    public void user_should_not_be_able_to_click_continue_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Then("user should be able to click continue button and landing to {string} page")
+    public void user_should_be_able_to_click_continue_button_and_landing_to_page(String expectedTitle) {
+        userInfoPage.continueButton.click();
+        Assert.assertEquals(expectedTitle, userInfoPage.title.getText());
     }
+
+    @When("user should be able to click continue button")
+    public void user_should_be_able_to_click_continue_button() {
+        userInfoPage.continueButton.click();
+    }
+
     @When("user should see the err message show {string}")
-    public void user_should_see_the_err_message_show(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_should_see_the_err_message_show(String expectedError) {
+        Assert.assertEquals(expectedError,userInfoPage.firstErrorMessage.getText());
     }
+
     @When("click close error message and re-enter more information")
     public void click_close_error_message_and_re_enter_more_information() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        userInfoPage.closeErrorButton.click();
     }
 
     @When("user enter {string} as first name and click continue button")
-    public void user_enter_as_first_name_and_click_continue_button(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_enter_as_first_name_and_click_continue_button(String firstName) {
+        BrowserUtil.waitForVisibility(userInfoPage.firstName,2);
+        userInfoPage.firstName.sendKeys(firstName);
     }
 
-    @Then("user should be able to click cancel button and go back to cart page")
-    public void user_should_be_able_to_click_cancel_button_and_go_back_to_cart_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Then("user should see last name err message show {string}")
+    public void user_should_see_last_name_err_message_show(String expectedError) {
+        Assert.assertEquals(expectedError,userInfoPage.lastNameErrorMessage.getText());
+    }
+
+    @Then("user should see zipcode err message show {string}")
+    public void user_should_see_zipcode_err_message_show(String expectedError) {
+        Assert.assertEquals(expectedError,userInfoPage.zipCodeErrorMessage.getText());
+    }
+
+    @Then("user should be able to click cancel button and go back to cart page {string} title")
+    public void user_should_be_able_to_click_cancel_button_and_go_back_to_cart_page_title(String expectedTitle) {
+        userInfoPage.cancelButton.click();
+        Assert.assertEquals(expectedTitle, userInfoPage.title.getText());
     }
 }
--- src/test/resources/features/UsersInformation.feature
+++ src/test/resources/features/UsersInformation.feature
@@ -1,5 +1,6 @@
 
-Feature: user can can remove the item on the Cart page
+Feature: On the Checkout: Your Information page, verify that all fields are required.
+  AC: user should be able to enter First name, Last name, and Zipcode as required
 
   Background: For all scenario, user is on the Cart page
     Given user is on the product page
@@ -11,26 +12,26 @@ Feature: user can can remove the item on the Cart page
   Scenario: user enter all information details as required
     When user enter "Jhon" as first name and "Cena" as last name
     And user enter valid zip code 94566
-    Then user should be able to click continue button and landing to checkout overview page
+    Then user should be able to click continue button and landing to "Checkout: Overview" page
 
   Scenario: user not enter first name or all information as required
-    And user should not be able to click continue button
+    And user should be able to click continue button
     When user should see the err message show "Error: First Name is required"
     And click close error message and re-enter more information
     When user enter "Chris" as first name and "Hemsworth" as last name
     And user enter valid zip code 94987
-    Then user should be able to click continue button and landing to checkout overview page
+    Then user should be able to click continue button and landing to "Checkout: Overview" page
 
   #negative
   Scenario: user enter only first name but not enter last name and zip code
     When user enter "Jhon" as first name and click continue button
-    And user should not be able to click continue button
-    Then user should see the err message show "Error: Last Name is required"
+    Then user should be able to click continue button
+    Then user should see last name err message show "Error: Last Name is required"
     And click close error message and re-enter more information
 
   Scenario: user enter only first name and last name but not enter zip code
     When user enter "Jhon" as first name and "Cena" as last name
-    And user should not be able to click continue button
-    Then user should see the err message show "Error: Postal Code is required"
-    And user should be able to click cancel button and go back to cart page
+    And user should be able to click continue button
+    Then user should see zipcode err message show "Error: Postal Code is required"
+    And user should be able to click cancel button and go back to cart page "Your Cart" title
 
